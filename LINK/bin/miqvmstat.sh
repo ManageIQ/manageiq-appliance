7036f911becbb56f5a61cf0cb155acf222c12fe9
dmetzger <dmetzger@redhat.com>
2015-06-17 08:06:20 -0400

    Migrated top(1) and vmstat(1) logging to systemd control

--- LINK/bin/miqvmstat.sh
+++ LINK/bin/miqvmstat.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-### Description: miqtop starts top(1) sending output to /var/www/miq/vmdb/log/top_output.log` ###
+### Description: miqvmstat starts vmstat(1) sending output to /var/www/miq/vmdb/log/vmstat_output.log` ###
 
 LOG_DIR=/var/www/miq/vmdb/log
 PID_DIR=/var/www/miq/vmdb/tmp/pids
 
-LOG_FILE=${LOG_DIR}/top_output.log
-PID_FILE=${PID_DIR}/top.pid
+LOG_FILE=${LOG_DIR}/vmstat_output.log
+PID_FILE=${PID_DIR}/vmstat.pid
 
 ### Attempt to create pid and log directores in case they do not exist ###
 mkdir -p ${PID_DIR}
 mkdir -p ${LOG_DIR}
 chown ${USER}:${USER} ${PID_DIR} ${LOG_DIR}
 
-### If an instance of top(1) in running, do not start another. ###
+### If an instance of vmstat(1) in running, do not start another. ###
 if [ -e ${PID_FILE} ]
 then
 	PID=`cat ${PID_FILE}`
 	ps -p $PID
 	if [ $? -eq 0 ]
 	then
-		echo "miqtop: top already started $PID"
+		echo "miqvmstat: vmstat already started $PID"
 		exit 1
 	else
 		### PID file exists but process is dead, delete the PID file ###
@@ -28,6 +28,6 @@ then
 	fi
 fi
 
-echo "miqtop: start: date time is-> $(date) $(date +%z)" >> ${LOG_FILE}
-TERM=dumb COLUMNS=512 top -b -d 60 >> ${LOG_FILE} 2>&1 &
+echo "miqvmstat: start: date time is-> $(date) $(date +%z)" >> ${LOG_FILE}
+vmstat -a -n 60 >> ${LOG_FILE} 2>&1 &
 echo $! >> ${PID_FILE}
