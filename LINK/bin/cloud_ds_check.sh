51eb3c16f51e0b845949f0245884cb757b2c169a
Nick Carboni <ncarboni@redhat.com>
2015-09-16 16:51:46 -0400

    Added systemd service to determine what cloud-init datasources to use
    The service runs before cloud-init and executes a script
    which writes the datasources to /etc/cloud/cloud.cfg.d/datasources.cfg

---
+++ LINK/bin/cloud_ds_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -I --connect-timeout 2 http://169.254.169.254/2009-04-04/meta-data
+[ "$?" = "0" ] && echo "datasource_list: [ Ec2, None ]" > /etc/cloud/cloud.cfg.d/datasources.cfg && exit 0
+
+curl -I --connect-timeout 2 http://169.254.169.254/openstack/latest/meta_data.json
+[ "$?" = "0" ] && echo "datasource_list: [ OpenStack, None ]" > /etc/cloud/cloud.cfg.d/datasources.cfg && exit 0
+
+gateway=`route -n | grep "^0\.0\.0\.0.*" | awk '{print $2}'`
+curl -I --connect-timeout 2 "http://$gateway/latest/meta-data"
+[ "$?" = "0" ] && echo "datasource_list: [ CloudStack, None ]" > /etc/cloud/cloud.cfg.d/datasources.cfg && exit 0
+
+echo "datasource_list: [ NoCloud, AltCloud, None ]" > /etc/cloud/cloud.cfg.d/datasources.cfg
