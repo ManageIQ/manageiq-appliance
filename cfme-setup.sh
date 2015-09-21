5cd609db47e1cdbd23ba6ec48ed93eb5efba5e42
Alberto Bellotti <abellott@redhat.com>
2015-09-21 16:17:43 -0400

    [auth] Lookup LDAP Groups does not work with IDM
    - Adding an SELinux policy to allow the application to do sssd queries
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1158421

--- cfme-setup.sh
+++ cfme-setup.sh
@@ -16,6 +16,27 @@ popd
 # httpd needs to connect to backend workers on :3000 and :4000
 setsebool -P httpd_can_network_connect on
 
+# app needs an selinux policy to access sssd via dbus
+if [ -z "`/sbin/semodule -l | grep '^init_dbus_sssd '`" ]
+then
+  SMOD=/tmp/miq_init_dbus_sssd
+  cat - >$SMOD.te <<!END!
+module init_dbus_sssd 1.0;
+
+require {
+  type sssd_t;
+  type initrc_t;
+  class dbus send_msg;
+}
+
+allow sssd_t initrc_t:dbus send_msg;
+!END!
+  /usr/bin/checkmodule -M -m -o $SMOD.mod $SMOD.te
+  /usr/bin/semodule_package -m $SMOD.mod -o $SMOD.pp
+  /sbin/semodule -i $SMOD.pp
+  rm -f $SMOD.te $SMOD.mod $SMOD.pp
+fi
+
 # backup the default ssl.conf
 mv /etc/httpd/conf.d/ssl.conf{,.orig}
 
