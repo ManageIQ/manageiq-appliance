647ac9d407d3f474dd7898ef05d1b1e834dcad10
Joe Rafaniello <jrafanie@redhat.com>
2015-10-06 18:34:10 -0400

    lib moved to vmdb/gems/pending since vmdb is the rails app.

--- LINK/usr/bin/evm_watchdog.rb
+++ LINK/usr/bin/evm_watchdog.rb
@@ -2,7 +2,7 @@
 # description: ManageIQ watchdog application loop
 #
 
-require '/var/www/miq/lib/util/system/evm_watchdog'
+require '/var/www/miq/vmdb/gems/pending/util/system/evm_watchdog'
 
 
 EvmWatchdog.kill_other_watchdogs # To prevent duplicates.
