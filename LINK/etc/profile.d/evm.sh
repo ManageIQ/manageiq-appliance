3296ae0f016b030496376dcefcd1457ac874222d
Joe VLcek <jvlcek@redhat.com>
2016-04-06 13:15:05 -0400

    Create alias for `appliance_console` command on appliance
    Addresses Trello card:
    https://trello.com/c/0exG9Pca/193-1-create-alias-for-appliance-console-command-on-appliance

--- LINK/etc/profile.d/evm.sh
+++ LINK/etc/profile.d/evm.sh
@@ -1,5 +1,6 @@
 [[ -s "/etc/default/evm" ]] && source "/etc/default/evm"
 
 # Aliases:
+alias ad='/usr/bin/appliance_console'
 alias vmdb='cd /var/www/miq/vmdb'
 alias appliance='[[ -n ${APPLIANCE_SOURCE_DIRECTORY} ]] && cd ${APPLIANCE_SOURCE_DIRECTORY}'
