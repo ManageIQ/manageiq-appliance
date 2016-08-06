d35c8e67615165f0fd7933037f658ff7304058ed
Mike Dahlgren <dahlgren@gmail.com>
2016-08-06 00:37:34 -0500

    removed railsc command

--- LINK/etc/profile.d/evm.sh
+++ LINK/etc/profile.d/evm.sh
@@ -5,15 +5,12 @@ alias ap='/usr/bin/appliance_console'
 alias vmdb='cd /var/www/miq/vmdb'
 alias appliance='[[ -n ${APPLIANCE_SOURCE_DIRECTORY} ]] && cd ${APPLIANCE_SOURCE_DIRECTORY}'
 
-# Tail MIQ Logs:
-function tailmiq()
+# Tail Logs:
+function tailmiq() #If no value is given with tailmiq it defaults to the evm.log
 {
   tail -f /var/www/miq/vmdb/log/${1:-evm}.log
 }
 alias tailpglog='tail -f $APPLIANCE_PG_DATA/pg_log/postgresql.log'
-
-# Rails Console:
-alias railsc="cd /var/www/miq/vmdb;echo '\$evm = MiqAeMethodService::MiqAeService.new(MiqAeEngine::MiqAeWorkspaceRuntime.new)'; rails c"
 
 # Appliance Status:
 alias apstatus='echo "EVM Status:";service evmserverd status;echo " ";echo "HTTP Status:";service httpd status'
