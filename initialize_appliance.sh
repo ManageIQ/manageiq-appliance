f0414e2fb9b4859d0392eda968d9935927a1c517
Nick Carboni <ncarboni@redhat.com>
2017-04-27 09:19:38 -0400

    Merge pull request #119 from isimluk/cve-2016-4457
    Generate new certificate when the default one is not present
    (cherry picked from commit 387fbf60ceae897838789dd97f737063955c1899)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1390815

--- initialize_appliance.sh
+++ initialize_appliance.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+set -e -o pipefail
 
-[[ ! -f "/var/www/miq/vmdb/certs/v2_key" ]] && appliance_console_cli --key
+KEYPATH="/var/www/miq/vmdb/certs"
+
+[[ ! -f "$KEYPATH/v2_key" ]] && appliance_console_cli --key
+
+CERT="$KEYPATH/server.cer"
+KEY="$CERT.key"
+if [ ! -f "$CERT" -a ! -f "$KEY" ]; then
+  (umask 077 ; openssl req -x509 -newkey rsa -days 1095 -keyout $KEY -out $CERT -subj "/CN=server" -nodes -batch)
+fi
