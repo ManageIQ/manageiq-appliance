2f7c41c58bbd7a770f75769ce1a9d75901f4a0bb
Brandon Dunne <bdunne@redhat.com>
2017-10-18 14:32:53 -0400

    Remove scripts that are no longer necessary with the new gem
    Also update ap alias

--- LINK/etc/profile.d/evm.sh
+++ LINK/etc/profile.d/evm.sh
@@ -1,7 +1,7 @@
 [[ -s "/etc/default/evm" ]] && source "/etc/default/evm"
 
 # Aliases:
-alias ap='/usr/bin/appliance_console'
+alias ap='appliance_console'
 alias vmdb='cd /var/www/miq/vmdb'
 alias appliance='[[ -n ${APPLIANCE_SOURCE_DIRECTORY} ]] && cd ${APPLIANCE_SOURCE_DIRECTORY}'
 
