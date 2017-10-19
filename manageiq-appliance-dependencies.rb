d73a1a2bd5b81164884d47d4869b9a53360366f4
Brandon Dunne <bdunne@redhat.com>
2017-10-19 18:12:04 -0400

    Use the released version of the manageiq-appliance_console gem

--- manageiq-appliance-dependencies.rb
+++ manageiq-appliance-dependencies.rb
@@ -1,2 +1,2 @@
 # Add gems here, in Gemfile syntax, which are dependencies of the appliance itself rather than a part of the ManageIQ application
-gem "manageiq-appliance_console", "~>0.1.0", :require => false, :git => "https://github.com/ManageIQ/manageiq-appliance_console.git", :branch => "master"
+gem "manageiq-appliance_console", "~>1.0.1", :require => false
