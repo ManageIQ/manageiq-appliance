d8fb50b722a11f15c300045dae1377de8b4cc3db
Bo Yao <icerove@gmail.com>
2017-10-25 18:39:20 -0400

    missing space

--- LINK/usr/bin/pg_inspector_server.sh
+++ LINK/usr/bin/pg_inspector_server.sh
@@ -2,5 +2,5 @@
 # description: dump miq_server table to YAML file
 
 export PGPASSWORD="${PGPASSWORD:-smartvm}"
-[[-s /etc/default/evm]] && source /etc/default/evm
+[[ -s /etc/default/evm ]] && source /etc/default/evm
 /var/www/miq/vmdb/tools/pg_inspector.rb servers
