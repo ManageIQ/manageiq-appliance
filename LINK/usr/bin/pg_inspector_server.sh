388f806f38cc34482e8ba4991569406ac9642923
Bo Yao <icerove@gmail.com>
2017-10-25 17:53:18 -0400

    portably find ruby in cron

--- LINK/usr/bin/pg_inspector_server.sh
+++ LINK/usr/bin/pg_inspector_server.sh
@@ -2,4 +2,5 @@
 # description: dump miq_server table to YAML file
 
 export PGPASSWORD="${PGPASSWORD:-smartvm}"
+[[-s /etc/default/evm]] && source /etc/default/evm
 /var/www/miq/vmdb/tools/pg_inspector.rb servers
