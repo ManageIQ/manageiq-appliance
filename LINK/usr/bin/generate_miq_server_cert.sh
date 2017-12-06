c46cc00f751df859d11d6962fe5b5ca1955471fe
Nick Carboni <ncarboni@redhat.com>
2017-12-06 11:38:03 -0500

    Use ENV variables to specify the cert and key file in the generate script
    This will allow us to generate new postgres certs with the following command:
    
    NEW_CERT_FILE=/var/www/miq/vmdb/certs/postgres.crt \
    NEW_KEY_FILE=/var/www/miq/vmdb/certs/postgres.key \
    generate_miq_server_cert.sh

--- LINK/usr/bin/generate_miq_server_cert.sh
+++ LINK/usr/bin/generate_miq_server_cert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e -o pipefail
 
-CERT="/var/www/miq/vmdb/certs/server.cer"
-KEY="$CERT.key"
+CERT="${NEW_CERT_FILE:-/var/www/miq/vmdb/certs/server.cer}"
+KEY="${NEW_KEY_FILE:-$CERT.key}"
 if [ ! -f "$CERT" -a ! -f "$KEY" ]; then
   (umask 077 ; openssl req -x509 -newkey rsa -days 1095 -keyout $KEY -out $CERT -subj "/CN=server" -nodes -batch)
 fi
