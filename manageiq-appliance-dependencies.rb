57cf8880414ce69ca35d100118e76a819ebb0c5d
Brandon Dunne <brandondunne@hotmail.com>
2018-01-18 18:23:10 -0500

    Merge pull request #177 from carbonin/console_1_2_4
    Increase console version to 1.2.4
    (cherry picked from commit 71a8c7e06eb9137611365cb0ba06dfbec382ac54)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1536227

--- manageiq-appliance-dependencies.rb
+++ manageiq-appliance-dependencies.rb
@@ -1,3 +1,3 @@
 # Add gems here, in Gemfile syntax, which are dependencies of the appliance itself rather than a part of the ManageIQ application
-gem "manageiq-appliance_console", "~>1.2", ">=1.2.3", :require => false
+gem "manageiq-appliance_console", "~>1.2", ">=1.2.4", :require => false
 gem "manageiq-postgres_ha_admin", "~>1.0.0",          :require => false
