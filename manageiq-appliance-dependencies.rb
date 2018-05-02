c56df73314f300a54d7e03e8e7a545725cf0a6d1
Nick Carboni <ncarboni@redhat.com>
2018-05-02 15:17:01 -0400

    Update the included console version to 2.0.2
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1561075

--- manageiq-appliance-dependencies.rb
+++ manageiq-appliance-dependencies.rb
@@ -1,3 +1,3 @@
 # Add gems here, in Gemfile syntax, which are dependencies of the appliance itself rather than a part of the ManageIQ application
-gem "manageiq-appliance_console", "~>2.0", ">=2.0.1", :require => false
+gem "manageiq-appliance_console", "~>2.0", ">=2.0.2", :require => false
 gem "manageiq-postgres_ha_admin", "~>1.0.0",          :require => false
