5c7697e98d2576a8a11aa874c4aea0dfe4598f60
Jason Frey <fryguy9@gmail.com>
2020-09-14 15:17:11 -0400

    Add RPM repo manifest

--- LINK/usr/bin/generate_rpm_manifest.sh
+++ LINK/usr/bin/generate_rpm_manifest.sh
@@ -4,6 +4,10 @@ manifest_dir=$APPLIANCE_SOURCE_DIRECTORY/../manifest
 mkdir -p $manifest_dir
 
 pushd $manifest_dir
+  #
+  # Generate RPM manifest
+  #
+
   manifest=rpm_manifest_$(date +"%m%d%y").csv
 
   # dnf command gives yum repo used to install rpm
@@ -23,4 +27,24 @@ pushd $manifest_dir
   join -j 1 -o 1.1,1.2,1.3,2.3 -t $'\t' -a 1 rpm_list dnf_list >> $manifest
   sed -i 's/\t/,/g' $manifest
   rm -f rpm_list dnf_list
+
+  #
+  # Generate RPM repo manifest
+  #
+  repo_manifest=rpm_repo_manifest_$(date +"%m%d%y").csv
+
+  dnf repolist -v enabled | ruby -r csv -e "$(cat <<- EORUBY
+    CSV(STDOUT) do |csv|
+      csv << %w[id name filename baseurl mirrors metalink]
+      STDIN
+        .grep(/^Repo-/)
+        .map { |line| line.chomp.split(":", 2).map(&:strip) }
+        .slice_when { |_, (k, _)| k == "Repo-id" }
+        .map { |repo| repo.to_h.values_at(*%w[Repo-id Repo-name Repo-filename Repo-baseurl Repo-mirrors Repo-metalink]) }
+        .sort_by { |repo| repo.first.downcase }
+        .each { |repo| csv << repo }
+    end
+EORUBY
+  )" > $repo_manifest
+
 popd
