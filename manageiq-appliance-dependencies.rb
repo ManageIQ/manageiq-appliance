9b16a69696cb8b4fa43cd953cbbba05c3cc2fd43
Brandon Dunne <brandondunne@hotmail.com>
2022-02-16 15:45:59 -0500

    Move appliance dependencies to core

--- manageiq-appliance-dependencies.rb
+++
@@ -1,2 +0,0 @@
-# Add gems here, in Gemfile syntax, which are dependencies of the appliance itself rather than a part of the ManageIQ application
-gem "manageiq-appliance_console", "~>7.0", ">=7.0.6", :require => false
