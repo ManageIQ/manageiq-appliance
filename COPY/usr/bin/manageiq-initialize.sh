c1da4a235a3bd6495e7cd89dfdc0c9b35e6c2e6e
Adam Grare <adam@grare.com>
2022-02-18 12:16:12 -0500

    Disable automatic configuration of kafka
    Kafka is not currently used by MIQ and the version installed contains a
    security vulnerability.  Disable automatic configuration of kafka for
    messaging for now.

--- COPY/usr/bin/manageiq-initialize.sh
+++ COPY/usr/bin/manageiq-initialize.sh
@@ -3,4 +3,3 @@
 [[ -s /etc/default/evm ]] && source /etc/default/evm
 echo "Initializing Appliance, please wait ..." > /dev/tty1
 appliance_console_cli --region 0 --internal --password smartvm --key
-appliance_console_cli --message-server-config --message-server-use-ipaddr --message-keystore-username="admin" --message-keystore-password="smartvm"
