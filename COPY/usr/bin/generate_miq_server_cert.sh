64021a9f1165a998752f25fe9f701af1a4665c29
Adam Grare <adam@grare.com>
2022-08-25 09:54:26 -0400

    Give group only read permissions

--- COPY/usr/bin/generate_miq_server_cert.sh
+++ COPY/usr/bin/generate_miq_server_cert.sh
@@ -5,5 +5,5 @@ CERT="${NEW_CERT_FILE:-/var/www/miq/vmdb/certs/server.cer}"
 KEY="${NEW_KEY_FILE:-$CERT.key}"
 if [ ! -f "$CERT" -a ! -f "$KEY" ]; then
   openssl req -x509 -newkey rsa -days 1095 -keyout $KEY -out $CERT -subj "/CN=server" -nodes -batch
-  chmod 0660 $CERT $KEY
+  chmod 0640 $CERT $KEY
 fi
